158d7c620748b147430abade925d2b502d2c28a6
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-01-19 08:24:55 +0530

    Update Jump_Search.cpp

--- Jump_Search.cpp
+++ Jump_Search.cpp
@@ -2,6 +2,12 @@
 #include<cmath>
 using namespace std;
 
+/* In Jump search we take the Square root of the no. of elements and then jump through the array with those steps like if we have an array of length 16 then the 
+jump size is sqrt(16) ,i.e., 4 so now we jump in the array with jumpsize = 4. After this we check whether the element at the jumped point is greater than our target
+element or not, if the element is less than our target element then we move further in the array. But when we come to a point where the element at the current position
+is greater than the target element, we jump backwards ,i.e., if we were at 12th position and the element was greater than our target so we move back to 8th position and
+do a linear search from 8th index. */
+
 int jumpsearch(int *arr,int high,int target){
     int low = 0;
 
